f1d357924160ac939941337f581821e6ce7f8029
raselASrock <mdraselm413@gmail.com>
2022-09-05 23:56:35 +0600

    info

---
+++ promt.js
@@ -0,0 +1,20 @@
+console.log('promot added');
+// alert('ki khobor dada?')
+const showAlart = () => {
+    const num = Math.random()*10;
+    console.log(num);
+    if( num < 5){
+        alert('Dada kmn aso???')
+    }
+}
+
+const askSomething = () => {
+    const decession = confirm('Are you commig to the picnic?')
+    console.log(decession)
+    if( decession === true){
+        alert('500 tk bkash koro');
+    }
+    else{
+        console.log('Dure jaiya Morek');
+    }
+}
