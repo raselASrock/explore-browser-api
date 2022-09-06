10e3981b0d09b90c05cc37adf5ec69ea0729af27
raselASrock <mdraselm413@gmail.com>
2022-09-06 23:25:09 +0600

    local code

--- js/storage.js
+++ js/storage.js
@@ -4,6 +4,23 @@ document.getElementById('btn-add-name').addEventListener('click', function(){
     const nameValue = nameField.value 
     console.log(nameValue);
     localStorage.setItem('Name', nameValue)
-})
+});
 
-localStorage.getItem('name')
+document.getElementById('btn-add-age').addEventListener('click', function(){
+    const ageField = document.getElementById('age-field');
+    const ageValue = ageField.value;
+    console.log(ageValue);
+    localStorage.setItem('Age', ageValue)
+});
+
+document.getElementById('btn-remove-name').addEventListener('click', function(){
+    localStorage.removeItem('Name');
+});
+
+document.getElementById('btn-remove-age').addEventListener('click', function(){
+    localStorage.removeItem('Age');
+});
+
+document.getElementById('btn-clear-LS').addEventListener('click', function(){
+    localStorage.clear()
+});
